6d6299f9845d67b8854d81303f828e43a190a83a
Aditya Shrivastava <adityashrivastava2005@gmail.com>
2023-10-28 10:14:57 +0530

    Create elements.sh

---
+++ elements.sh
@@ -0,0 +1,10 @@
+# Input string
+input_string=" "
+
+# Getting the 3rd and 6th element
+third_element="${input_string:2:1}"  #substring extraction operation 
+sixth_element="${input_string:5:1}"  
+
+# Printing the results
+echo "3rd element: $third_element"
+echo "6th element: $sixth_element"
